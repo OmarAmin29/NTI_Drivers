5c55fa811add5b09e2a00ed3852fe8356d0264e7
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-21 12:16:04 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 20, 2023
+ *      Author: lenovo
+ */
+
+#include "../MCAL/DIO/Dio.h"
+
+int main()
+{
+    DIO_PrephieralType* port = ( DIO_PrephieralType*)0x39;
+
+    while(1)
+    {
+    	DIO_SetPinDirection(port, DIO_PIN5, DIO_DIRECTION_OUTPUT);
+        DIO_SetPinValue(port, DIO_PIN5, DIO_VALUE_HIGH);
+
+    }
+
+
+}
 
