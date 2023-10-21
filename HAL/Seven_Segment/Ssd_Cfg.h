3300798190bf7d2825a8992d13b0be1c92749d7d
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-21 15:35:52 +0300

    Create Ssd_Cfg.h

---
+++ HAL/Seven_Segment/Ssd_Cfg.h
@@ -0,0 +1,21 @@
+/*
+ * Ssd_Cfg.h
+ *
+ *  Created on: Oct 21, 2023
+ *      Author: lenovo
+ */
+
+#ifndef HAL_SEVEN_SEGMENT_SSD_CFG_H_
+#define HAL_SEVEN_SEGMENT_SSD_CFG_H_
+
+#define ENABLE     1
+#define DISABLE    0
+
+#define SEVEN_SEGEMENT_1   	ENABLE
+#define SEVEN_SEGEMENT_2   	ENABLE
+#define SEVEN_SEGEMENT_3   	DISABLE
+#define SEVEN_SEGEMENT_4   	ENABLE
+
+
+
+#endif /* HAL_SEVEN_SEGMENT_SSD_CFG_H_ */
