fd09f7cc3bbba03f51e5fef15be58172ac6e7e57
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-21 15:35:27 +0300

    Create Ssd.h

---
+++ HAL/Seven_Segment/Ssd.h
@@ -0,0 +1,17 @@
+/*
+ * Ssd.h
+ *
+ *  Created on: Oct 21, 2023
+ *      Author: lenovo
+ */
+
+#ifndef HAL_SEVEN_SEGMENT_SSD_H_
+#define HAL_SEVEN_SEGMENT_SSD_H_
+
+#include "../../Private/Math.h"
+
+void SS_Init(void);
+void SS_DisplayNumber(u8 number);
+
+
+#endif /* HAL_SEVEN_SEGMENT_SSD_H_ */
