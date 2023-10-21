5afbe90add4440e9035a0bbdbb14dbc1da841469
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-21 15:42:36 +0300

    Update Dio_Cfg.h

--- MCAL/DIO/Dio_Cfg.h
+++ MCAL/DIO/Dio_Cfg.h
@@ -9,11 +9,10 @@
 #define DIO_DIO_CFG_H_
 
 
-#define PERIPHERAL   		*((volatile DIO_PrephieralType *)0x30)
-#define PORTA               *((volatile DIO_PrephieralType *)0x39)                
-#define PORTB               *((volatile DIO_PrephieralType *)0x36)               
-#define PORTC               *((volatile DIO_PrephieralType *)0x33)                   
-#define PORTD               *((volatile DIO_PrephieralType *)0x30)
+#define PORTA               ( DIO_PrephieralType *)0x39)                
+#define PORTB               ( DIO_PrephieralType *)0x36)               
+#define PORTC               ( DIO_PrephieralType *)0x33)                   
+#define PORTD               ( DIO_PrephieralType *)0x30)
 
 
 #endif /* DIO_DIO_CFG_H_ */
