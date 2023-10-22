c8d60457847942d89b4ad9c378af4d1b346c7438
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-22 22:17:16 +0300

    Create Keypad_Priv.h

---
+++ HAL/Keypad/Keypad_Priv.h
@@ -0,0 +1,13 @@
+/*
+ * Keypad_Priv.h
+ *
+ *  Created on: Oct 22, 2023
+ *      Author: lenovo
+ */
+
+#ifndef HAL_KEYPAD_KEYPAD_PRIV_H_
+#define HAL_KEYPAD_KEYPAD_PRIV_H_
+
+
+
+#endif /* HAL_KEYPAD_KEYPAD_PRIV_H_ */
