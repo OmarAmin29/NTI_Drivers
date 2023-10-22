123e8c11fb82c5030230d5ee28e83e800a9ff9b3
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-22 22:11:23 +0300

    Create Gie_Priv.h

---
+++ MCAL/Global_Interrupt/Gie_Priv.h
@@ -0,0 +1,14 @@
+/*
+ * Gie_Priv.h
+ *
+ *  Created on: Oct 22, 2023
+ *      Author: lenovo
+ */
+
+#ifndef MCAL_GENERAL_INTERRUPT_GIE_PRIV_H_
+#define MCAL_GENERAL_INTERRUPT_GIE_PRIV_H_
+
+#define SREG		*((volatile u8*) 0x5F)
+#define SREG_I      7
+
+#endif /* MCAL_GENERAL_INTERRUPT_GIE_PRIV_H_ */
