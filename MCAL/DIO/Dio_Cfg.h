776a84f7cc6e5e8c4c3215ed225a8c3c5f2bc884
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-22 22:07:05 +0300

    Update Dio_Cfg.h

--- MCAL/DIO/Dio_Cfg.h
+++ MCAL/DIO/Dio_Cfg.h
@@ -9,10 +9,6 @@
 #define DIO_DIO_CFG_H_
 
 
-#define PORTA               ( DIO_PrephieralType *)0x39)                
-#define PORTB               ( DIO_PrephieralType *)0x36)               
-#define PORTC               ( DIO_PrephieralType *)0x33)                   
-#define PORTD               ( DIO_PrephieralType *)0x30)
 
 
 #endif /* DIO_DIO_CFG_H_ */
