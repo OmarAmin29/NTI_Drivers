b7a72980c8e83f0c2489f629e2565831ad99e776
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-22 22:03:28 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,21 +5,37 @@
  *      Author: lenovo
  */
 
+#include "../Private/Types.h"
 #include "../MCAL/DIO/Dio.h"
 #include "../HAL/Seven_Segment/Ssd.h"
+#include "../MCAL/General_Interrupt/Gie.h"
+#include "../MCAL/EXTINT/Extint.h"
+#include"Delay.h"
+
+void Handler_EXT_Int0 (void)
+{
+   static u8 counter = 0;
+    if(counter % 2 == 0)
+    DIO_SetPinValue(DIO_PORTD, DIO_PIN6, DIO_VALUE_HIGH);
+    else
+    {
+        DIO_SetPinValue(DIO_PORTD, DIO_PIN6, DIO_VALUE_LOW);
+    }
+    counter++;
+}
+
 
 
 int main()
 {
+    DIO_SetPinDirection(DIO_PORTD, DIO_PIN6, DIO_DIRECTION_OUTPUT);
 
-	SS_Init();
-    DIO_SetPinDirection(DIO_PORTA, DIO_PIN6, DIO_DIRECTION_OUTPUT);
-
-    while(1)
+    Extint_voidEnable(EXTINT_ID_INT0,EXTINT_TRIGGER_EDGE_FALLING);
+    Extint_SetCallback(EXTINT_ID_INT0,Handler_EXT_Int0);
+    Gie_Enable();
+    
+    while (1)
     {
-    	SS_DisplayNumber(0);
-
-        DIO_SetPinValue(DIO_PORTA, DIO_PIN6, DIO_VALUE_HIGH);
+            
     }
-
 }
