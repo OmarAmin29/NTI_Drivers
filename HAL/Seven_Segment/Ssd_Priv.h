c9fd8284937cb09d03453b1087e1692cd1ee8390
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-22 22:14:42 +0300

    Create Ssd_Priv.h

---
+++ HAL/Seven_Segment/Ssd_Priv.h
@@ -0,0 +1,13 @@
+/*
+ * Ssd_Priv.h
+ *
+ *  Created on: Oct 22, 2023
+ *      Author: lenovo
+ */
+
+#ifndef HAL_SEVEN_SEGMENT_SSD_PRIV_H_
+#define HAL_SEVEN_SEGMENT_SSD_PRIV_H_
+
+
+
+#endif /* HAL_SEVEN_SEGMENT_SSD_PRIV_H_ */
