783aefcaf995701eda9d50ae996674310c84c799
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-22 22:06:20 +0300

    Create Dio_Priv.h

---
+++ MCAL/DIO/Dio_Priv.h
@@ -0,0 +1,18 @@
+/*
+ * Dio_Priv.h
+ *
+ *  Created on: Oct 22, 2023
+ *      Author: lenovo
+ */
+
+#ifndef MCAL_DIO_DIO_PRIV_H_
+#define MCAL_DIO_DIO_PRIV_H_
+
+
+#define DIO_PORTA                ( DIO_PrephieralType *)0x39
+#define DIO_PORTB                ( DIO_PrephieralType *)0x36
+#define DIO_PORTC                ( DIO_PrephieralType *)0x33
+#define DIO_PORTD                ( DIO_PrephieralType *)0x30
+
+
+#endif /* MCAL_DIO_DIO_PRIV_H_ */
