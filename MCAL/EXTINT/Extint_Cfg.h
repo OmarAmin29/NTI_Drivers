25bfb753863eea77d43f1dfb7412311fab46708a
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-10-22 22:09:59 +0300

    Create Extint_Cfg.h

---
+++ MCAL/EXTINT/Extint_Cfg.h
@@ -0,0 +1,13 @@
+/*
+ * Extint_Cfg.h
+ *
+ *  Created on: Oct 22, 2023
+ *      Author: lenovo
+ */
+
+#ifndef MCAL_EXTINT_EXTINT_CFG_H_
+#define MCAL_EXTINT_EXTINT_CFG_H_
+
+
+
+#endif /* MCAL_EXTINT_EXTINT_CFG_H_ */
