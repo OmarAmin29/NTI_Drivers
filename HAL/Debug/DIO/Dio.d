9f72245d4d4dd6673cfd2665f5bef4ee6ca5efe3
OmarAmin29 <139606656+OmarAmin29@users.noreply.github.com>
2023-11-21 04:59:40 +0200

    Delete HAL/Debug directory

--- HAL/Debug/DIO/Dio.d
+++
@@ -1,8 +0,0 @@
-DIO/Dio.o DIO/Dio.o: ../DIO/Dio.c ../DIO/Dio.h ../DIO/../Private/Math.h \
-  ../DIO/Dio_Cfg.h
-
-../DIO/Dio.h:
-
-../DIO/../Private/Math.h:
-
-../DIO/Dio_Cfg.h:
--- HAL/Debug/src/main.d
+++
@@ -1,8 +0,0 @@
-src/main.o src/main.o: ../src/main.c ../src/../DIO/Dio.h \
-  ../src/../DIO/../Private/Math.h ../src/../DIO/Dio_Cfg.h
-
-../src/../DIO/Dio.h:
-
-../src/../DIO/../Private/Math.h:
-
-../src/../DIO/Dio_Cfg.h:
